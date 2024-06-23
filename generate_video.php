836205a2572051c20b52972b66c8cf9866534403
Yoshiroobesato <webapi.000.pe@gmail.com>
2024-06-23 03:49:04 -0400

    Update and rename video_api.php to generate_video.php

---
+++ generate_video.php
@@ -0,0 +1,10 @@
+<?php
+function generateHLSVideoUrl() {
+    // Aquí puedes implementar la lógica para generar y firmar la URL del video HLS
+    // Por ejemplo, puedes tener una función que firme la URL y devolverla aquí
+    return 'https://z8dpsptc.sw-cdnstreamwish.com/hls2/01/03974/8zen94yqm5jy_,n,h,.urlset/master.m3u8?t=YUVOeVtZ1W9vvAxeciKW2GH9l_ASSC8juwRlJR-wX3U&s=1719126381&e=129600&f=20565819&srv=xhisossxof6ziadr&i=0.4&sp=500&p1=xhisossxof6ziadr&p2=xhisossxof6ziadr&asn=28024';
+}
+
+// Obtener la URL del video HLS
+$videoUrl = generateHLSVideoUrl();
+?>
